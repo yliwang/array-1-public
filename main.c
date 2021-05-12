e8663a7873747983153f4cf4362933b90a848161
yliwang <1220264221@qq.com>
2021-05-12 17:03:05 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,14 @@
 #include <stdio.h>
 
-#include "linked_string.h"
-
-struct string *c_to_string(struct linked_string *s, char *c_str)
-{
-	int i, len = strlen(c_str);
-
-	if (len == 0) return NULL;
-
-	for (i = 0; i < len; i++) {
-		add_tail(s, c_str[i]);
-	}
-
-	return s;
-}
-
+#include "array.h"
 
 void main()
 {
-	struct linked_string txt;
-	struct linked_string pat;
-	int index;
-
-	init_string(&txt);
-	init_string(&pat);
-
-	c_to_string(&txt, "ABC ABCDAB ABCDABCDABDE");
-	c_to_string(&pat, "ABCDABD");
-
-	index = index_of(&txt, &pat);
-	printf("index:%d\n", index);
-
+	struct array a;
+	init_array(&a, 5, 3 );
+		
 	 /* other test code */
 
 
-	uninit_string(&txt);
-	uninit_string(&pat);
-
-
+	uninit_array(&a);
 }
